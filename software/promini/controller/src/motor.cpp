1c2eae93f5970ef36ff31c4a8063f96ab44ec6d9
Ben <andreabenini@users.noreply.github.com>
2023-03-31 22:38:38 +0200

    motor controller library

---
+++ software/promini/controller/src/motor.cpp
@@ -0,0 +1,24 @@
+#include "motor.h"
+
+
+// TODO: Build this
+/**
+ * motor - Control motor movements
+ * @param motorNumber (uint8_t) 0: all motors, 1: motor1, 2: motor2
+ * @param dutyCycle   (uint8_t) 0..100 duty cycle on power
+ * @param forward     (bool)    true:forward, false:backward
+ */
+void motor(uint8_t motorNumber, int8_t dutyCycle, bool forward=true) {
+    switch (motorNumber) {
+    case 0:
+        motor(1, dutyCycle, forward);
+        motor(2, dutyCycle, forward);
+        return;
+    case 1:
+        Serial.print(motorNumber);
+        break;
+    case 2:
+        Serial.print(motorNumber);
+        break;
+    }    
+} /**/
