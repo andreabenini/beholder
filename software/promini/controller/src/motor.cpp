4a09e716eecc0d61ce5c1df4062e7414a95c285a
Ben <andreabenini@users.noreply.github.com>
2024-02-22 18:49:50 +0100

    motor controller software with 8833

--- software/promini/controller/src/motor.cpp
+++ software/promini/controller/src/motor.cpp
@@ -1,28 +1,60 @@
 #include "motor.h"
 
 
-// TODO: Build this
+/**
+ * motorInit - Init motor control circuit
+ */
+void motorInit() {
+    // Motor control inputs to OUTPUT
+    pinMode(MOTOR1_IN_1, OUTPUT);
+    pinMode(MOTOR1_IN_2, OUTPUT);
+    pinMode(MOTOR2_IN_3, OUTPUT);
+    pinMode(MOTOR2_IN_4, OUTPUT);
+    // Turn off motors - Initial state
+    digitalWrite(MOTOR1_IN_1, LOW);
+    digitalWrite(MOTOR1_IN_2, LOW);
+    digitalWrite(MOTOR2_IN_3, LOW);
+    digitalWrite(MOTOR2_IN_4, LOW);
+} /**/
+
+
+void motorPWM(int8_t dutyCycle, uint8_t pin1, uint8_t pin2) {
+    char buffer[200];
+
+    uint8_t pwm = round(abs(2.55 * dutyCycle));
+    if (dutyCycle < 0) {        // Backward
+        digitalWrite(pin1, LOW);
+        analogWrite(pin2, pwm);
+        sprintf(buffer, "\npin1[%d]:LOW, pin2[%d]:%d", pin1, pin2, pwm);
+    } else {                    // Stop or forward
+        digitalWrite(pin1, pwm);
+        analogWrite(pin2, LOW);
+        sprintf(buffer, "\npin1[%d]:%d, pin2[%d]:LOW", pin1, pwm, pin2);
+    }
+    Serial.println(buffer);
+} /**/
+
+
 /**
  * motor - Control motor movements
  * @param motorNumber (uint8_t) 0: all motors, 1: motor1, 2: motor2
- * @param dutyCycle   (uint8_t) 0..100 duty cycle on power
- * @param forward     (bool)    true:forward, false:backward
+ * @param dutyCycle   (int8_t)  Duty cycle percentage.  0..100: forward, -100..0: backward
  */
-void motor(uint8_t motorNumber, int8_t dutyCycle, bool forward=true) {
+void motor(uint8_t motorNumber, int8_t dutyCycle) {
     switch (motorNumber) {
     case 0:
-        motor(1, dutyCycle, forward);
-        motor(2, dutyCycle, forward);
-        return;
+        motorPWM(dutyCycle, MOTOR1_IN_1, MOTOR1_IN_2);
+        motorPWM(dutyCycle, MOTOR2_IN_3, MOTOR2_IN_4);
+        break;
     case 1:
-        Serial.print(motorNumber);
+        motorPWM(dutyCycle, MOTOR1_IN_1, MOTOR1_IN_2);
         break;
     case 2:
-        Serial.print(motorNumber);
+        motorPWM(dutyCycle, MOTOR2_IN_3, MOTOR2_IN_4);
         break;
     }
-    
+    // DEBUG from here
     char buffer[200];
-    sprintf(buffer, "Motor:%d, duty:%d, direction:%d", motor, dutyCycle, forward);
+    sprintf(buffer, "Motor:%d, duty:%d.", motorNumber, dutyCycle);
     Serial.println(buffer);
 } /**/
--- software/promini/controller/src/motor.h
+++ software/promini/controller/src/motor.h
@@ -3,6 +3,14 @@
 #include <Arduino.h>
 
 
-void motor(uint8_t motorNumber, int8_t dutyCycle, bool forward=true);
+// Motor control input pinout
+#define MOTOR1_IN_1     9
+#define MOTOR1_IN_2     6
+#define MOTOR2_IN_3     5
+#define MOTOR2_IN_4     3
+
+
+void motorInit();
+void motor(uint8_t motorNumber, int8_t dutyCycle);
 
 #endif
