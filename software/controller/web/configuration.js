ebf3b5db8e90b0cd2be53b1db339bdabd38c16b2
Ben <andreabenini@users.noreply.github.com>
2024-04-05 15:42:20 +0200

    Configuration parameters for dealing with the robot
    Please change SERVER according to your needs (here hardcoded for testing purposes)

---
+++ software/controller/web/configuration.js
@@ -0,0 +1,16 @@
+// bot server
+const SERVER            = "192.168.0.230"
+
+// Cursor appearance
+const CURSORCOLORSHADOW = '#ECE5E5';
+const CURSORCOLOROUTER  = '#F6ABAB';
+const CURSORCOLORINNER  = '#F08080';
+
+// Cursor movement tolerance
+const CURSORTOLERANCE   = 1;
+
+// Cursor radius from the center
+const RADIUS            = 200;
+
+// Min/Max speed value (DutyCycle to apply)
+const THROTTLE          = 100;
