5b6788591b29264d9630e48644cc549e332dc3d7
Ben <andreabenini@users.noreply.github.com>
2024-04-25 23:16:23 +0200

    New display library for I2C oled display

---
+++ software/nanomicro/controller/src/display.h
@@ -0,0 +1,14 @@
+#ifndef _DISPLAY__H_
+#define _DISPLAY__H_
+// #include <U8g2lib.h>
+#include "SSD1306Ascii.h"
+#include "SSD1306AsciiAvrI2c.h"
+
+
+#include "icons.h"
+
+void displayInit();
+void displayEyes(byte image);
+
+
+#endif
